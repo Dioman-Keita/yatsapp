8a34c59529d1f668d758ceea86ab58f25b8d52e5
ibrahima_Sidibe <fillfi773@gmail.com>
2026-05-02 21:15:02 +0000

    all user not session user

--- app/all_member/page.tsx
+++ app/all_member/page.tsx
@@ -1,11 +1,14 @@
-"use client";
 import { Suspense } from "react";
-import { useUser } from "../store/user.store";
 import AllUsersLoading from "./components/allUsersLoading";
 import AllUsersContent from "./components/allUsersContent";
+import { auth } from "../lib/auth";
+import { headers } from "next/headers";
 
-export default function ListUsersPage() {
-    const { userID } = useUser();
+export default async function ListUsersPage() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    const userID = session?.user.id;
     return (
         <div className="h-full text-white flex flex-col overflow-hidden">
             <Suspense fallback={<AllUsersLoading />}>
